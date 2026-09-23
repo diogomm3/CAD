4ffba01d0534db16e4d0ee1fa87ee734e75ba24e
diogomm3 <diogomm@gmail.com>
2026-09-24 00:20:34 +0100

    Remove Thingiverse from backend/app/scrapers/registry.py

--- backend/app/scrapers/registry.py
+++ backend/app/scrapers/registry.py
@@ -1,6 +1,5 @@
 from .printables import PrintablesSource
 from .makerworld import MakerWorldSource
-from .thingiverse import ThingiverseSource
 from .grabcad import GrabCADSource
 
-SOURCES = {s.key: s for s in (PrintablesSource(), MakerWorldSource(), ThingiverseSource(), GrabCADSource())}
+SOURCES = {s.key: s for s in (PrintablesSource(), MakerWorldSource(), GrabCADSource())}
