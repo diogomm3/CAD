5aa17df8d3a1eb8214ed97dc8d0fafeb7a1dc318
diogomm3 <diogomm@gmail.com>
2026-09-25 13:02:09 +0100

    Remove GrabCAD from source registry

--- backend/app/scrapers/registry.py
+++ backend/app/scrapers/registry.py
@@ -1,5 +1,4 @@
 from .printables import PrintablesSource
 from .makerworld import MakerWorldSource
-from .grabcad import GrabCADSource
 
-SOURCES = {s.key: s for s in (PrintablesSource(), MakerWorldSource(), GrabCADSource())}
+SOURCES = {s.key: s for s in (PrintablesSource(), MakerWorldSource())}
